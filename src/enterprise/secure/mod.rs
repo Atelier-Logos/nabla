29e3ca89a5067de8846a5b5a070b2456669f0589
James Bohrman <james@getoasis.io>
2025-08-05 08:59:54 -0400

    fix exports

--- src/enterprise/secure/mod.rs
+++ src/enterprise/secure/mod.rs
@@ -7,6 +7,7 @@ pub mod supply_chain;
 // Re-export main analysis functions for easy access
 pub use behavioral_analysis::analyze_behavioral_security;
 pub use crypto_analysis::analyze_crypto_security;
+pub use control_flow::build_cfg;
 pub use static_analysis::analyze_static_security;
 pub use supply_chain::analyze_supply_chain_security;
 
